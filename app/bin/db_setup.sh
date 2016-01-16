ab0ab69670dadb602c54232a0fa8bc5dc05bbf59
Tiago Maia Lopes <tiago.maia@adjetiva.com.br>
2016-01-16 20:54:34 -0200

    Adding db_setup shell script

---
+++ app/bin/db_setup.sh
@@ -0,0 +1,11 @@
+#!/bin/shell
+#
+# Shell script used to setup the App DB
+#
+
+composer dumpautoload
+
+sudo -u postgres psql -c "DROP DATABASE swiss;"
+sudo -u postgres psql -c "CREATE DATABASE swiss OWNER homestead;"
+
+php artisan migrate
