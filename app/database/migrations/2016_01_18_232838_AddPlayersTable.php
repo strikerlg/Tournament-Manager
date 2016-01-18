5b78ffc66b6a73ed1acfae48f3aa92a07c96b0f6
Tiago Maia Lopes <tiago.maia@adjetiva.com.br>
2016-01-18 21:50:11 -0200

    Adding players migration

---
+++ app/database/migrations/2016_01_18_232838_AddPlayersTable.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPlayersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('players', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('nickname', 60);
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('players');
+    }
+}
