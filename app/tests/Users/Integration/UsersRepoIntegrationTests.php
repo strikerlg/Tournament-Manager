438b7707ff9255811d3b018025b940658413337c
Tiago Maia Lopes <tiago.maia@adjetiva.com.br>
2016-01-20 23:56:54 -0200

    Adding User repository initial test

---
+++ app/tests/Users/Integration/UsersRepoIntegrationTests.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Users\Integration;
+
+/**
+ * Class for testing the users repo.
+ */
+class UsersRepoIntegrationTest extends TestCase
+{
+    /**
+     * Basic is working assertion.
+     */
+    public function testIsWorking()
+    {
+        $this->assertTrue(true);
+    }
+
+    /**
+     * Tests the User addition function within the repository.
+     */
+    public function testAddUserSuccess()
+    {
+        $user = $this->repo->addUser(
+            'user@mail.com',
+            'pass'
+        );
+        $this->assertNotNull($user);
+        $this->assertInstanceOf($user, 'App\Models\User');
+        $this->seeInDatabase('users', ['user@mail.com']);
+    }
+
+}
+
+
+
+
+
