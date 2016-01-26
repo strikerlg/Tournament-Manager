16e822dfa35de89d807135c0abb44dd16021a2db
Tiago Maia Lopes <tiago.maia@adjetiva.com.br>
2016-01-26 05:41:49 -0200

    Adding factory code for the match model.

--- app/tests/factories/factories.php
+++ app/tests/factories/factories.php
@@ -1,5 +1,7 @@
 <?php
 
+use Laracasts\TestDummy\Factory;
+
 $factory('App\\Models\\User', [
     'name' => $faker->name,
     'email' => $faker->email,
@@ -23,7 +25,7 @@ $factory('App\\Models\\Game', [
 $factory('App\\Models\\Tournament', [
     'name' => $faker->name,
     'begin' => $faker->dateTimeBetween(
-        'now', '+ 3 days'
+        'now', '+3 days'
     ),
     'finish' => $faker->dateTimeBetween(
         '+3 days', '+3 days'
@@ -31,4 +33,23 @@ $factory('App\\Models\\Tournament', [
     'has_ended' => false,
     'created_by' => 'factory:App\\Models\\Administrator',
 ]);
+
+$factory('App\\Models\\Match', function($faker) {
+    $tournament = Factory::create('App\\Models\\Tournament');
+    $admin = $tournament->createdBy;
+
+    return [
+        'tournament_id' => $tournament,
+        'first_player_id' => 'factory:App\\Models\\Player',
+        'second_player_id' => 'factory:App\\Models\\Player',
+        'winner' => 'factory:App\\Models\\Player',
+        'begin' => $faker->dateTimeBetween(
+            'now', '+2 days'
+        ),
+        'finish' => $faker->dateTimeBetween(
+            '+2 days', '+2 days'
+        ),
+        'created_by' => $admin,
+    ];
+});
 
