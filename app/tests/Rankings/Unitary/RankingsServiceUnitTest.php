88a3bb6332b9d9e0056a75714455c286d07661d3
Tiago Maia Lopes <tiago.maia@adjetiva.com.br>
2016-02-01 15:20:57 -0200

    adding test for the addRanking method in the rankings service

--- app/tests/Rankings/Unitary/RankingsServiceUnitTest.php
+++ app/tests/Rankings/Unitary/RankingsServiceUnitTest.php
@@ -23,6 +23,11 @@ class RankingsServiceUnitTest extends \TestCase
     protected $fakePlayersRepo;
 
     /**
+     * @var Mockery
+     */
+    protected $fakeTournamentsRepo;
+
+    /**
      * Setup method.
      */
     public function setup()
@@ -31,12 +36,16 @@ class RankingsServiceUnitTest extends \TestCase
         $this->fakeRankingsRepo = m::mock(
             'App\\Repositories\\Rankings\\IRankingsRepository'
         );
-        $this->fakeRankingsRepo = m::mock(
+        $this->fakePlayersRepo = m::mock(
             'App\\Repositories\\Players\\IPlayersRepository'
         );
+        $this->fakeTournamentsRepo = m::mock(
+            'App\\Repositories\\Tournaments\\ITournamentsRepository'
+        );
         $this->service = new RankingsService(
-            $this->fakeRankingsService,
-            $this->fakePlayersService
+            $this->fakeRankingsRepo,
+            $this->fakePlayersRepo,
+            $this->fakeTournamentsRepo
         );
     }
 
@@ -46,6 +55,57 @@ class RankingsServiceUnitTest extends \TestCase
     public function testIsWorking()
     {
         $this->assertTrue(true);
+    }
+
+    /**
+     * Tests if the addRanking has the
+     * expected flow.
+     */
+    public function testServiceAddRankingCorrectInteractions()
+    {
+        $playerID = 1;
+        $tournamentID = 1;
+        $score = 0;
+
+        // TODO: Add administrator fake instance.
+        $fakeTournament = m::mock(
+            'App\\Models\\Tournament'
+        );
+        $fakePlayer = m::mock(
+            'App\\Models\\Player'
+        );
+        $fakeRanking = m::mock(
+            'App\\Models\\Ranking'
+        );
+
+        $this->fakeTournamentsRepo
+            ->shouldReceive('getTournament')
+            ->with($tournamentID)
+            ->once()
+            ->andReturn($fakeTournament);
+        $this->fakePlayersRepo
+            ->shouldReceive('getPlayer')
+            ->with($playerID)
+            ->once()
+            ->andReturn($fakePlayer);
+        $this->fakeRankingsRepo
+            ->shouldReceive('addRanking')
+            ->withArgs([
+                m::type('App\\Models\\Player'),
+                m::type('App\\Models\\Tournament'),
+                $score,
+            ])
+            ->once()
+            ->andReturn($fakeRanking);
+        $match = $this->service->addMatch(
+            $tournamentID,
+            $playerID,
+            $score
+        );
+        $this->assertEquals(
+            $match,
+            $fakeMatch
+        );
     }
 }
 
