dd00b33dee95e49a1b5f3f6e43ff71bd2834723b
Tiago Maia Lopes <tiago.maia@adjetiva.com.br>
2016-02-11 22:48:01 -0200

    adding new replicated test for the users registrastration route

---
+++ app/tests/Players/Integration/PlayersRoutesIntegrationTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Players\Integration;
+
+class PlayersRoutesIntegrationTest extends TestCase
+{
+    /**
+     * Test is working.
+     */
+    public function testIsWorking()
+    {
+        $this->assertTrue(true);
+    }
+
+    /**
+     * Tests if a player can register
+     * correctly.
+     */
+    public function testRegisterPlayerPlayerRegistered()
+    {
+        $playerData = [
+            'name' => 'player',
+            'email' => 'player@player.test',
+            'password' => '123456',
+            'password_confirm' => '123456',
+        ];
+
+        $response = $this->assertOk(
+            '/player/register',
+            'POST',
+            $playerData
+        );
+
+        $this->assertNotNull($response->player);
+        $this->seeInDatabase('players', [
+            'name' => 'player',
+        ]);
+    }
+
+}
+
